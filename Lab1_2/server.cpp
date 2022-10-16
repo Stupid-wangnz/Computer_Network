3b7637f31683e25fe987343029e9cfc15d2ce172
Stupid-wangnz <81021995+Stupid-wangnz@users.noreply.github.com>
2022-10-16 15:12:54 +0800

    Lab1 improve
    实现双人（客户端）聊天功能

--- Lab1_2/server.cpp
+++ Lab1_2/server.cpp
@@ -9,34 +9,34 @@
 #define PORT 7878
 #define BUF_SIZE 512
 #define ADDRSRV "127.0.0.1"
-int CLIENTNUM=2;
 using namespace std;
+
+SOCKET sockConnects[2];
 
 DWORD WINAPI handlerTransfer(LPVOID lparam) {
     SOCKET *socket = (SOCKET *) lparam;
     char buf[BUF_SIZE];
     memset(buf, 0, BUF_SIZE);
+    int i = (socket == &sockConnects[0]) ? 0 : 1;
     while (1) {
-        for (int i = 0; i < CLIENTNUM; i++) {
-            int len = recv(socket[i], buf, BUF_SIZE, 0);
-            if (strcmp(buf, "quit") == 0) {
-                SYSTEMTIME sysTime;
-                GetLocalTime(&sysTime);
-                printf("%4d/%02d/%02d %02d:%02d:%02d.%03d 星期%1d\n", sysTime.wYear, sysTime.wMonth, sysTime.wDay,
-                       sysTime.wHour, sysTime.wMinute, sysTime.wSecond, sysTime.wMilliseconds, sysTime.wDayOfWeek);
-                printf("用户%d已经退出聊天室",i);
-                send(socket[(i+1)%2],buf, strlen(buf),0);
-            }
-            if (len > 0) {
-                SYSTEMTIME sysTime;
-                GetLocalTime(&sysTime);
-                printf("%4d/%02d/%02d %02d:%02d:%02d.%03d 星期%1d\n", sysTime.wYear, sysTime.wMonth, sysTime.wDay,
-                       sysTime.wHour, sysTime.wMinute, sysTime.wSecond, sysTime.wMilliseconds, sysTime.wDayOfWeek);
-                printf("用户%d:%s\n", i,buf);
-                send(socket[(i+1)%2],buf, strlen(buf),0);
-            }
-            memset(buf, 0, BUF_SIZE);
+        int len = recv(*socket, buf, BUF_SIZE, 0);
+        if (strcmp(buf, "quit") == 0) {
+            SYSTEMTIME sysTime;
+            GetLocalTime(&sysTime);
+            printf("%4d/%02d/%02d %02d:%02d:%02d.%03d 星期%1d\n", sysTime.wYear, sysTime.wMonth, sysTime.wDay,
+                   sysTime.wHour, sysTime.wMinute, sysTime.wSecond, sysTime.wMilliseconds, sysTime.wDayOfWeek);
+            printf("用户%d已经退出聊天室", i);
+            send(sockConnects[(i + 1) % 2], buf, strlen(buf), 0);
         }
+        if (len > 0) {
+            SYSTEMTIME sysTime;
+            GetLocalTime(&sysTime);
+            printf("%4d/%02d/%02d %02d:%02d:%02d.%03d 星期%1d\n", sysTime.wYear, sysTime.wMonth, sysTime.wDay,
+                   sysTime.wHour, sysTime.wMinute, sysTime.wSecond, sysTime.wMilliseconds, sysTime.wDayOfWeek);
+            printf("用户%d:%s\n", i, buf);
+            send(sockConnects[(i + 1) % 2], buf, strlen(buf), 0);
+        }
+        memset(buf, 0, BUF_SIZE);
     }
 }
 
@@ -62,14 +62,11 @@ int main() {
         return -1;
     }
 
-    SOCKET sockConnects[2];
-
     SOCKADDR_IN addrClient1;
     int lenAddr = sizeof(SOCKADDR);
     sockConnects[0] = accept(sockServer, (SOCKADDR *) &addrClient1, &(lenAddr));
-    if (sockConnects[0]== SOCKET_ERROR) {
+    if (sockConnects[0] == SOCKET_ERROR) {
         cout << "连接失败" << endl;
-        getchar();
         return -1;
     }
     cout << "客户端1连接成功,它的IP是:" << inet_ntoa(addrClient1.sin_addr) << endl;
@@ -79,16 +76,17 @@ int main() {
     sockConnects[1] = accept(sockServer, (SOCKADDR *) &addrClient2, &(lenAddr));
     if (sockConnects[1] == SOCKET_ERROR) {
         cout << "连接失败" << endl;
-        getchar();
         return -1;
     }
     cout << "客户端2连接成功,它的IP是:" << inet_ntoa(addrClient2.sin_addr) << endl;
 
-    HANDLE hthread;
-    hthread = CreateThread(NULL, NULL, handlerTransfer, (LPVOID) sockConnects, 0, NULL);
-    WaitForMultipleObjects(1, &hthread, TRUE, INFINITE);
-    CloseHandle(hthread);
-    
+    HANDLE hthread[2];
+    hthread[0] = CreateThread(NULL, NULL, handlerTransfer, (LPVOID) &sockConnects[0], 0, NULL);
+    hthread[1] = CreateThread(NULL, NULL, handlerTransfer, (LPVOID) &sockConnects[1], 0, NULL);
+    WaitForMultipleObjects(2, hthread, TRUE, INFINITE);
+    CloseHandle(hthread[0]);
+    CloseHandle(hthread[1]);
+
     closesocket(sockConnects[0]);
     closesocket(sockConnects[1]);
     closesocket(sockServer);
